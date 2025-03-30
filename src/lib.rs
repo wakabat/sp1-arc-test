b8900cec1242dd5807f14b33f7730ed9f323de31
wakabat <wakabat@protonmail.com>
2025-03-30 09:51:34 +0000

    Fix by upgrading rkyv

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,14 @@ use std::sync::Arc;
 use rkyv::Archive;
 
 #[derive(Archive, Default, Debug)]
-#[archive_attr(derive(Debug))]
+#[rkyv(derive(Debug))]
 pub struct Inner {
     pub a: usize,
     pub b: usize,
 }
 
 #[derive(Archive, Default, Debug)]
-#[archive_attr(derive(Debug))]
+#[rkyv(derive(Debug))]
 pub struct Outer {
     pub inner: Arc<Inner>,
 }
